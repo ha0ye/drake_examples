ceab9c3af2f3c690e169790bc466555ef73ccad7
wlandau-lilly <will.landau@lilly.com>
2019-04-26 13:31:53 -0400

    Suggest a different approach to the pipeline

--- pipeline.R
+++ pipeline.R
@@ -1,73 +1,66 @@
-library(tidyverse)
+# packages ---------------------------------------------------
+
 library(drake)
+library(tidyverse)
 
-# utility functions ------------------------------------------------------------
+# functions ---------------------------------------------------
 
-collect_analyses <- function(list_of_results)
-{
-    names(list_of_results) <- all.vars(match.call()$list_of_results)
-    list_of_results
+bytes <- function(x) as.numeric(object.size(x))
+
+elements <- function(x) {
+  if (!is.null(dim(x))) {
+    prod(dim(x))  
+  } else {
+    length(x)  
+  }
 }
 
-build_analyses_plan <- function(methods, datasets, ...)
-{
-    drake::drake_plan(
-        analysis = target(fun(data),
-                          transform = cross(fun = !!rlang::syms(methods$target),
-                                            data = !!rlang::syms(datasets$target))
-        ),
-        results = target(collect_analyses(list(analysis)),
-                         transform = combine(analysis, .by = fun)),
-        ...
-    )
+vis <- function(full) {
+  ggplot(full) +
+    geom_line(aes(x = data, y = value, color = method, group = method)) +
+    facet_wrap(~method, scales = "free_y", ncol = 1) +
+    theme_bw()
 }
 
-# drake plans ------------------------------------------------------------------
+# plan ---------------------------------------------------
 
-datasets <- drake::drake_plan(
-    mtcars = mtcars, 
-    iris = iris, 
-    Nile = Nile, 
-    sunspots = sunspots
+datasets <- c("mtcars", "iris", "Nile", "sunspots") %>%
+  rlang::syms()
+
+pipeline <- drake_plan(
+  data = target(x, transform = map(x = !!datasets)),
+  analysis = target(
+    tibble(
+      value = fun(data),
+      method = deparse(substitute(fun)),
+      data = deparse(substitute(data))
+    ),
+    transform = cross(data, fun = c(bytes, elements))
+  ),
+  results = target(
+    bind_rows(analysis),
+    transform = combine(analysis, .by = fun)
+  ),
+  full = target(
+    bind_rows(results),
+    transform = combine(results)
+  ),
+  plot = vis(full),
+  report = rmarkdown::render(
+    knitr_in("report.Rmd"),
+    output_file = file_out("report.html"),
+    quiet = TRUE
+  )
 )
 
-fun_wrapper <- function(f)
-{
-    method_name <- all.vars(match.call()$f)
-    function(x)
-    {
-        dataset_name <- all.vars(match.call()$x)
-        data.frame(value = as.numeric(f(x)),
-                   method = method_name, 
-                   dataset = dataset_name)
-    }
+# visuals ---------------------------------------------------
+
+if (FALSE) {
+  config <- drake_config(pipeline)
+  sankey_drake_graph(config, build_times = "none")  # requires "networkD3" package
+  vis_drake_graph(config, build_times = "none")     # requires "visNetwork" package
 }
 
-methods <- drake::drake_plan(
-    nrow = fun_wrapper(NROW),
-    ncol = fun_wrapper(NCOL),
-    mem = fun_wrapper(object.size)
-)
-
-analyses <- build_analyses_plan(methods, datasets)
-
-reports <- drake_plan(
-    report = rmarkdown::render(
-        knitr_in("report.Rmd")
-    )
-)
-
-pipeline <- bind_rows(datasets, methods, analyses, reports)
-
-# View the graph of the plan ---------------------------------------------------
-
-if (interactive())
-{
-    config <- drake_config(pipeline)
-    sankey_drake_graph(config, build_times = "none")  # requires "networkD3" package
-    vis_drake_graph(config, build_times = "none")     # requires "visNetwork" package
-}
-
-# Run the pipeline -------------------------------------------------------------
+# execution -------------------------------------------------------------
 
 make(pipeline)
